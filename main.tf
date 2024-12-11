1bca8b738b729092cd34860dfa3fe899f8df2e6e
Diviyasm <158040561+Diviyasm@users.noreply.github.com>
2024-12-11 14:53:14 +0530

    Update main.tf
    last commit

--- main.tf
+++ main.tf
@@ -2,31 +2,30 @@
 provider "aws" {
   region = "us-west-2"
 }
-
 # VPC
 resource "aws_vpc" "my_app_vpc" {
   cidr_block = "10.0.0.0/16"
+  
 }
 
 # Subnets
 resource "aws_subnet" "my_public_subnet_1" {
-  vpc_id            = aws_vpc.my_app_vpc.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-west-2a"
-  map_public_ip_on_launch = true
+  vpc_id                  = aws_vpc.my_app_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-west-2a"
+  
 }
 
 resource "aws_subnet" "my_public_subnet_2" {
-  vpc_id            = aws_vpc.my_app_vpc.id
-  cidr_block        = "10.0.2.0/24"
-  availability_zone = "us-west-2b"
-  map_public_ip_on_launch = true
+  vpc_id                  = aws_vpc.my_app_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-west-2b"
 }
 
 resource "aws_subnet" "my_private_subnet" {
-  vpc_id            = aws_vpc.my_app_vpc.id
-  cidr_block        = "10.0.3.0/24"
-  availability_zone = "us-west-2a"
+  vpc_id                  = aws_vpc.my_app_vpc.id
+  cidr_block              = "10.0.3.0/24"
+  availability_zone       = "us-west-2a"
 }
 
 # Internet Gateway
@@ -34,7 +33,7 @@ resource "aws_internet_gateway" "my_igw" {
   vpc_id = aws_vpc.my_app_vpc.id
 }
 
-# Route Table
+#Route Table
 resource "aws_route_table" "my_public_route_table" {
   vpc_id = aws_vpc.my_app_vpc.id
   route {
@@ -43,7 +42,7 @@ resource "aws_route_table" "my_public_route_table" {
   }
 }
 
-# Associate Route table with Subnets
+#Associate Route table with Subnets
 resource "aws_route_table_association" "my_public_subnet_1" {
   subnet_id      = aws_subnet.my_public_subnet_1.id
   route_table_id = aws_route_table.my_public_route_table.id
@@ -91,20 +90,27 @@ resource "aws_security_group" "my_private_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  
 }
 
-# Autoscaling Group
+# Launch_template
 resource "aws_launch_template" "my_app_launch_template" {
   name          = "my-app-launch-template"
   instance_type = "t2.micro"
   image_id      = "ami-055e3d4f0bbeb5878"
-}
+  iam_instance_profile {
+    name = aws_iam_instance_profile.my_app_role_profile.name
+  }
+  vpc_security_group_ids = [aws_security_group.my_public_sg.id]
+} 
 
+
+ # Autoscaling Group
 resource "aws_autoscaling_group" "my_app_asg" {
-  desired_capacity    = 2
-  max_size            = 5
-  min_size            = 2
-  vpc_zone_identifier = [aws_subnet.my_public_subnet_1.id, aws_subnet.my_public_subnet_2.id]
+  desired_capacity     = 2
+  max_size             = 5
+  min_size             = 2
+  vpc_zone_identifier  = [aws_subnet.my_public_subnet_1.id, aws_subnet.my_public_subnet_2.id]
   launch_template {
     id      = aws_launch_template.my_app_launch_template.id
     version = "$Latest"
@@ -113,31 +119,32 @@ resource "aws_autoscaling_group" "my_app_asg" {
 
 # Single EC2 Instance
 resource "aws_instance" "my_private_instance" {
-  ami                    = "ami-055e3d4f0bbeb5878"
-  instance_type          = "t2.micro"
-  subnet_id              = aws_subnet.my_private_subnet.id
+  ami           = "ami-055e3d4f0bbeb5878" 
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.my_private_subnet.id
   vpc_security_group_ids = [aws_security_group.my_private_sg.id]
+  
 }
 
 # Load Balancers
 resource "aws_lb" "my_app_alb" {
-  name               = "my-app-alb-unique"
-  internal           = false
+  name            = "my-app-alb"
+  internal        = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.my_public_sg.id]
-  subnets            = [aws_subnet.my_public_subnet_1.id, aws_subnet.my_public_subnet_2.id]
+  security_groups = [aws_security_group.my_public_sg.id]
+  subnets         = [aws_subnet.my_public_subnet_1.id, aws_subnet.my_public_subnet_2.id]
 }
 
 resource "aws_lb" "my_app_nlb" {
-  name               = "my-app-nlb-unique"
-  internal           = true
+  name            = "my-app-nlb"
+  internal        = true
   load_balancer_type = "network"
-  subnets            = [aws_subnet.my_private_subnet.id]
+  subnets         = [aws_subnet.my_private_subnet.id]
 }
 
 # Target Group for Application Load Balancer
 resource "aws_lb_target_group" "my_alb_target_group" {
-  name        = "my-alb-tg-unique"
+  name        = "my-alb-tg"
   port        = 80
   protocol    = "HTTP"
   vpc_id      = aws_vpc.my_app_vpc.id
@@ -156,15 +163,15 @@ resource "aws_lb_listener" "my_alb_listener" {
   }
 }
 
-# Auto Scaling Group Target Group Attachment for Application Load Balancer
+# Autoscaling Attachment
 resource "aws_autoscaling_attachment" "my_asg_alb_attachment" {
   autoscaling_group_name = aws_autoscaling_group.my_app_asg.name
-  lb_target_group_arn    = aws_lb_target_group.my_alb_target_group.arn
+  lb_target_group_arn = aws_lb_target_group.my_alb_target_group.arn
 }
 
 # Target Group for Network Load Balancer
 resource "aws_lb_target_group" "my_nlb_target_group" {
-  name        = "my-nlb-tg-unique"
+  name        = "my-nlb-tg"
   port        = 80
   protocol    = "TCP"
   vpc_id      = aws_vpc.my_app_vpc.id
@@ -191,12 +198,13 @@ resource "aws_lb_target_group_attachment" "my_nlb_instance_attachment" {
   port             = 80
 }
 
+
 # S3 Bucket
 resource "aws_s3_bucket" "my_app_bucket" {
-  bucket = "my-usecse-app-bucket-unique"
+  bucket = "my-usecse-app-bucket"
 }
 
-resource "aws_s3_bucket_acl" "my_app_bucket_acl" {
+resource "aws_s3_bucket_acl" "rohit_app_bucket_acl" {
   bucket = aws_s3_bucket.my_app_bucket.id
   acl    = "private"  # Apply private ACL
 }
@@ -209,9 +217,9 @@ resource "aws_s3_bucket_versioning" "versioning_bucket" {
   }
 }
 
-# IAM Role
+# IAM Role for ec2
 resource "aws_iam_role" "my_app_role" {
-  name = "my-app-role-unique"
+  name = "my-app-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
@@ -229,7 +237,7 @@ resource "aws_iam_role" "my_app_role" {
 
 # IAM Policy for S3 Access
 resource "aws_iam_policy" "my_bucket_access_policy" {
-  name        = "my-s3-access-policy-unique"
+  name        = "my-s3-access-policy"
   description = "Provide full access to S3 bucket"
 
   policy = jsonencode({
@@ -245,4 +253,15 @@ resource "aws_iam_policy" "my_bucket_access_policy" {
       }
     ]
   })
+}
+
+#Attaching IAM Role Policy
+resource "aws_iam_role_policy_attachment" "my_attach_s3_policy" {
+  role       = aws_iam_role.my_app_role.name
+  policy_arn = aws_iam_policy.my_bucket_access_policy.arn
+}
+# IAM Instance Profile
+resource "aws_iam_instance_profile" "my_app_role_profile" {
+  name = "my-app-role-profile"
+  role = aws_iam_role.my_app_role.name
 }
