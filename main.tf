bcfb7bd66e594babdbf09c7900b6709b7a2bdc26
Diviyasm <158040561+Diviyasm@users.noreply.github.com>
2024-12-11 16:52:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,76 +1,70 @@
-#Provider
+# Provider
 provider "aws" {
   region = "us-west-2"
 }
-# VPC
-resource "aws_vpc" "my_app_vpc" {
+
+# VPC Creation
+resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
-  
 }
 
-# Subnets
-resource "aws_subnet" "my_public_subnet_1" {
-  vpc_id                  = aws_vpc.my_app_vpc.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-west-2a"
+# Public Subnet Creation
+resource "aws_subnet" "public" {
+  count = 2
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 4, count.index)
   map_public_ip_on_launch = true
-  
+  availability_zone       = ["us-west-2a", "us-west-2b"][count.index]
 }
 
-resource "aws_subnet" "my_public_subnet_2" {
-  vpc_id                  = aws_vpc.my_app_vpc.id
-  cidr_block              = "10.0.2.0/24"
-  availability_zone       = "us-west-2b"
-  map_public_ip_on_launch = true
-
+# Private Subnet Creation
+resource "aws_subnet" "private" {
+  count = 2
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 4, 2 + count.index)
+  availability_zone = ["us-west-2a", "us-west-2b"][count.index]
 }
 
-resource "aws_subnet" "my_private_subnet" {
-  vpc_id                  = aws_vpc.my_app_vpc.id
-  cidr_block              = "10.0.3.0/24"
-  availability_zone       = "us-west-2a"
+# Internet Gateway Creation
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
 }
 
-# Internet Gateway
-resource "aws_internet_gateway" "my_igw" {
-  vpc_id = aws_vpc.my_app_vpc.id
-}
+# Route Table for Public Subnet
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
 
-#Route Table
-resource "aws_route_table" "my_public_route_table" {
-  vpc_id = aws_vpc.my_app_vpc.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.my_igw.id
+    gateway_id = aws_internet_gateway.main.id
   }
 }
 
-#Associate Route table with Subnets
-resource "aws_route_table_association" "my_public_subnet_1" {
-  subnet_id      = aws_subnet.my_public_subnet_1.id
-  route_table_id = aws_route_table.my_public_route_table.id
+# Route Table Association for Public Subnet
+resource "aws_route_table_association" "public" {
+  count          = 2
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
 }
 
-resource "aws_route_table_association" "my_public_subnet_2" {
-  subnet_id      = aws_subnet.my_public_subnet_2.id
-  route_table_id = aws_route_table.my_public_route_table.id
-}
-
-# Security Groups
+# Security Group for Public Subnet
 resource "aws_security_group" "my_public_sg" {
-  vpc_id = aws_vpc.my_app_vpc.id
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  vpc_id = aws_vpc.main.id
+
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
@@ -79,204 +73,187 @@ resource "aws_security_group" "my_public_sg" {
   }
 }
 
+# Security Group for Private Subnet
 resource "aws_security_group" "my_private_sg" {
-  vpc_id = aws_vpc.my_app_vpc.id
+  vpc_id = aws_vpc.main.id
+
   ingress {
     from_port       = 0
     to_port         = 0
     protocol        = "-1"
     security_groups = [aws_security_group.my_public_sg.id]
   }
+
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  
 }
 
-# Launch_template
-resource "aws_launch_template" "my_app_launch_template" {
-  name          = "my-app-launch-template"
+# Launch Template for Public EC2 Instances
+resource "aws_launch_template" "my_public_instance" {
+  name          = "my-public-instance-template"
   instance_type = "t2.micro"
-  image_id      = "ami-055e3d4f0bbeb5878"
+  image_id      = "ami-055e3d4f0bbeb5878" 
   iam_instance_profile {
-    name = aws_iam_instance_profile.my_app_role_profile.name
+    name = aws_iam_instance_profile.my_public_role.name
   }
   vpc_security_group_ids = [aws_security_group.my_public_sg.id]
-} 
+}
 
-
- # Autoscaling Group
-resource "aws_autoscaling_group" "my_app_asg" {
-  desired_capacity     = 2
-  max_size             = 5
-  min_size             = 2
-  vpc_zone_identifier  = [aws_subnet.my_public_subnet_1.id, aws_subnet.my_public_subnet_2.id]
+# Auto Scaling Group for Public EC2 Instances
+resource "aws_autoscaling_group" "my_public_asg" {
+  desired_capacity    = 2
+  max_size            = 3
+  min_size            = 1
+  vpc_zone_identifier = [
+    aws_subnet.public[0].id,
+    aws_subnet.public[1].id
+  ]
   launch_template {
-    id      = aws_launch_template.my_app_launch_template.id
+    id      = aws_launch_template.my_public_instance.id
     version = "$Latest"
   }
+  target_group_arns = [aws_lb_target_group.my_app_targets.arn]
 }
 
-# Single EC2 Instance
+# Private EC2 Instance
 resource "aws_instance" "my_private_instance" {
-  ami           = "ami-055e3d4f0bbeb5878" 
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.my_private_subnet.id
+  ami                    = "ami-055e3d4f0bbeb5878"
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.private[0].id
   vpc_security_group_ids = [aws_security_group.my_private_sg.id]
-  
 }
 
-# Load Balancers
-resource "aws_lb" "my_app_alb" {
-  name            = "my-app-alb"
-  internal        = false
+# Application Load Balancer
+resource "aws_lb" "my_application_lb" {
+  name               = "my-app-lb"
+  internal           = false
   load_balancer_type = "application"
-  security_groups = [aws_security_group.my_public_sg.id]
-  subnets         = [aws_subnet.my_public_subnet_1.id, aws_subnet.my_public_subnet_2.id]
-}
-
-resource "aws_lb" "my_app_nlb" {
-  name            = "my-app-nlb"
-  internal        = true
-  load_balancer_type = "network"
-  subnets         = [aws_subnet.my_private_subnet.id]
+  security_groups    = [aws_security_group.my_public_sg.id]
+  subnets            = [
+    aws_subnet.public[0].id,
+    aws_subnet.public[1].id
+  ]
 }
 
 # Target Group for Application Load Balancer
-resource "aws_lb_target_group" "my_alb_target_group" {
-  name        = "my-alb-tg"
-  port        = 80
-  protocol    = "HTTP"
-  vpc_id      = aws_vpc.my_app_vpc.id
-  target_type = "instance"
+resource "aws_lb_target_group" "my_app_targets" {
+  name     = "my-app-targets"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
 }
 
 # Listener for Application Load Balancer
-resource "aws_lb_listener" "my_alb_listener" {
-  load_balancer_arn = aws_lb.my_app_alb.arn
+resource "aws_lb_listener" "my_app_listener" {
+  load_balancer_arn = aws_lb.my_application_lb.arn
   port              = 80
   protocol          = "HTTP"
-
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.my_alb_target_group.arn
+    target_group_arn = aws_lb_target_group.my_app_targets.arn
   }
 }
 
-# Autoscaling Attachment
-resource "aws_autoscaling_attachment" "my_asg_alb_attachment" {
-  autoscaling_group_name = aws_autoscaling_group.my_app_asg.name
-  lb_target_group_arn = aws_lb_target_group.my_alb_target_group.arn
+# Network Load Balancer
+resource "aws_lb" "my_network_lb" {
+  name               = "my-net-lb"
+  internal           = true
+  load_balancer_type = "network"
+  subnets            = [
+    aws_subnet.private[0].id,
+    aws_subnet.private[1].id
+  ]
 }
 
 # Target Group for Network Load Balancer
-resource "aws_lb_target_group" "my_nlb_target_group" {
-  name        = "my-nlb-tg"
-  port        = 80
-  protocol    = "TCP"
-  vpc_id      = aws_vpc.my_app_vpc.id
-  target_type = "instance"
+resource "aws_lb_target_group" "my_network_targets" {
+  name     = "my-net-targets"
+  port     = 80
+  protocol = "TCP"
+  vpc_id   = aws_vpc.main.id
 }
 
 # Listener for Network Load Balancer
-resource "aws_lb_listener" "my_nlb_listener" {
-  load_balancer_arn = aws_lb.my_app_nlb.arn
+resource "aws_lb_listener" "my_net_listener" {
+  load_balancer_arn = aws_lb.my_network_lb.arn
   port              = 80
   protocol          = "TCP"
-
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.my_nlb_target_group.arn
+    target_group_arn = aws_lb_target_group.my_network_targets.arn
   }
 }
 
-# Target Group Attachment for Network Load Balancer
-# Private EC2 Instance
-resource "aws_lb_target_group_attachment" "my_nlb_instance_attachment" {
-  target_group_arn = aws_lb_target_group.my_nlb_target_group.arn
-  target_id        = aws_instance.my_private_instance.id
-  port             = 80
-}
-
-
 # S3 Bucket
-resource "aws_s3_bucket" "my_app_bucket" {
-  bucket = "diviya-bucket"
+resource "aws_s3_bucket" "my_private_bucket" {
+  bucket = "my-private-bucket"
 }
 
-# S3 Bucket Ownership Controls
-resource "aws_s3_bucket_ownership_controls" "my_app_bucket_ownership_controls" {
-  bucket = aws_s3_bucket.my_app_bucket_1.id
-
+resource "aws_s3_bucket_ownership_controls" "my_private_bucket_ownership_controls" {
+  bucket = aws_s3_bucket.my_private_bucket.id # Associates the ownership controls with the bucket
   rule {
-    object_ownership = "BucketOwnerPreferred"  # Object ownership set to bucket owner preferred
+    object_ownership = "BucketOwnerPreferred" # Sets the object ownership rule
   }
 }
 
 # S3 Bucket ACL
-resource "aws_s3_bucket_acl" "my_app_bucket_acl" {
-  depends_on = [aws_s3_bucket_ownership_controls.my_app_bucket_ownership_controls]  # Ensure ownership controls are created first
-
-  bucket = aws_s3_bucket.my_app_bucket_1.id
-  acl    = "private"  # Apply private ACL
+resource "aws_s3_bucket_acl" "my_private_bucket_acl" {
+  depends_on = [aws_s3_bucket_ownership_controls.my_private_bucket_ownership_controls] # Ensures ownership controls are created first
+  bucket = aws_s3_bucket.my_private_bucket.id # Associates the ACL with the bucket
+  acl = "private" # Applies private ACL
 }
 
-# S3 versioning
-resource "aws_s3_bucket_versioning" "versioning_bucket" {
-  bucket = aws_s3_bucket.my_app_bucket_1.id
+# S3 Bucket Versioning
+resource "aws_s3_bucket_versioning" "my_s3_versioning" {
+  bucket = aws_s3_bucket.my_private_bucket.id # Associates versioning with the bucket
   versioning_configuration {
-    status = "Enabled"
+    status = "Enabled" # Enables versioning
   }
 }
 
-# IAM Role for ec2
-resource "aws_iam_role" "my_app_role" {
-  name = "my-app-role"
-
+# IAM Role
+resource "aws_iam_role" "my_public_role" {
+  name               = "my-public-role"
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
       {
-        Effect = "Allow",
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        },
-        Action = "sts:AssumeRole"
+        Action    = "sts:AssumeRole",
+        Effect    = "Allow",
+        Principal = { Service = "ec2.amazonaws.com" }
       }
     ]
   })
 }
 
 # IAM Policy for S3 Access
-resource "aws_iam_policy" "my_bucket_access_policy" {
-  name        = "my-s3-access-policy"
-  description = "Provide full access to S3 bucket"
-
-  policy = jsonencode({
+resource "aws_iam_policy" "my_s3_access" {
+  name        = "my-s3-access-unique"
+  description = "Full access to the S3 bucket"
+  policy      = jsonencode({
     Version = "2012-10-17",
     Statement = [
       {
-        Effect   = "Allow",
         Action   = ["s3:*"],
-        Resource = [
-          aws_s3_bucket.my_app_bucket.arn,
-          "${aws_s3_bucket.my_app_bucket.arn}/*"
-        ]
+        Effect   = "Allow",
+        Resource = [aws_s3_bucket.my_private_bucket.arn, "${aws_s3_bucket.my_private_bucket.arn}/*"]
       }
     ]
   })
 }
 
-#Attaching IAM Role Policy
-resource "aws_iam_role_policy_attachment" "my_attach_s3_policy" {
-  role       = aws_iam_role.my_app_role.name
-  policy_arn = aws_iam_policy.my_bucket_access_policy.arn
+# Attach IAM Policy to Role
+resource "aws_iam_role_policy_attachment" "my_attach_policy" {
+  role       = aws_iam_role.my_public_role.name
+  policy_arn = aws_iam_policy.my_s3_access.arn
 }
+
 # IAM Instance Profile
-resource "aws_iam_instance_profile" "my_app_role_profile" {
-  name = "my-app-role-profile-2"
-  role = aws_iam_role.my_app_role.name
+resource "aws_iam_instance_profile" "my_public_role" {
+  name = "my-public-instance-profile-unique"
+  role = aws_iam_role.my_public_role.name
 }
