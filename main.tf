cdde1249aea56be098f81e293c95b446925027b5
Diviyasm <158040561+Diviyasm@users.noreply.github.com>
2024-12-11 14:56:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,6 +13,7 @@ resource "aws_subnet" "my_public_subnet_1" {
   vpc_id                  = aws_vpc.my_app_vpc.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "us-west-2a"
+  map_public_ip_on_launch = true
   
 }
 
@@ -20,6 +21,8 @@ resource "aws_subnet" "my_public_subnet_2" {
   vpc_id                  = aws_vpc.my_app_vpc.id
   cidr_block              = "10.0.2.0/24"
   availability_zone       = "us-west-2b"
+  map_public_ip_on_launch = true
+
 }
 
 resource "aws_subnet" "my_private_subnet" {
